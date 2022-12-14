4421732eef6598df2ea7113b7c41383b7ca52517
Lucy CPU <120491336+lucycpu@users.noreply.github.com>
2022-12-14 22:52:14 +0000

    added insert statement to sample data

--- 2 sample data.sql
+++ 2 sample data.sql
@@ -1,6 +1,8 @@
+delete germ
+
+insert germ(GermName, Host)
 select 'Norovirus', 'human'
 select 'Salmonella', 'plant'
 select 'Clostridium perfringens', 'human'
 select 'Campylobacter', 'animal'
 select 'Staphylococcus aureus', 'human'
-
